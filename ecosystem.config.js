9b2e8984e920d34bd3dea73030794464654dca4e
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-08-11 11:51:50 -0300

    Source Folder

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    apps: [{
-        name: "ActionsHasura",
-        script: "./app.js"
-    }]
-}
+    apps: [
+        {
+            name: "ActionsHasura",
+            script: "./src/app.js",
+        },
+    ],
+};
